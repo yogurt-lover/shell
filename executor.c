84edb961cc3a70ac24c5959a31c803945250266c
AWinnerIsYou <seijiyawata@gmail.com>
2016-11-21 11:48:06 -0500

    a

--- executor.c
+++ executor.c
@@ -5,24 +5,48 @@
 
 #define PROMPT "$ "
 
-int main() {
-      
+
+void process() {
   char input[100];
   printf(PROMPT);
   fgets(input, sizeof(input), stdin);
   input[strlen(input)-1] = 0; //remove newline
 
-  char * command[100];
   char *s = input;
-  char *p;
-
-  int i = 0;
   while (s) {
-    command[i] = strsep(&s, " ");
-    i++;
-  }
-  command[i] = 0;
+    char *single_input = strsep(&s, ";");
+    
+    char *command_args[100];
+    char *s2 = single_input;
+    
+    int i = 0;
+    while (s2) {
+      command_args[i] = strsep(&s2, " ");
+      i++;
+    }
+    command_args[i] = 0;
   
-  execvp(command[0], command);
+    int f = fork();
+    if (f == 0) {
+      printf("k\n");
+      execvp(command_args[0], command_args);
+    }
+    else {
+      wait();
+    }
+  }
+}
+
+int main() {
+  while (1) {
+    int f = fork();
+    if (f == 0) {
+      process();
+      return 0;
+    }
+    else {
+      wait();
+    }
+  }
   return 0;
 }
