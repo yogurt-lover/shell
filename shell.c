0974d2fcae9349fba9d1c315be79ca272c69f09f
yawata159 <seijiyawata@gmail.com>
2016-11-27 12:54:45 -0500

    Implement multiple <, >'s - "~" becomes home directory

--- shell.c
+++ shell.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <errno.h>
 #include <pwd.h>
+
 #include <sys/wait.h>
 #include <sys/types.h>
 
@@ -16,10 +16,14 @@
 // signals (Ctrl-C)
 // resizing buffers
 // redirection << < | > >>
-// cd -
-// print error messages
+// cd - (history)
+// error handling
 // previous command
 // config + builtins
+// *
+// multiple <, >
+// no need for new_std*?
+
 
 char *get_home_dir() {
 	struct passwd *pw = getpwuid(getuid());
@@ -33,13 +37,15 @@ char *get_username() {
 
 char *get_hostname() {
 	char *hostname = (char *)malloc(256*sizeof(char));
-	gethostname(hostname, 256);
+	hostname[256] = '\0';
+	gethostname(hostname, 255);
 	return hostname;
 }
 
 char *get_cwd() {
 	char *cwd = (char *)malloc(256*sizeof(char));
-	getcwd(cwd, 256);
+	cwd[256] = '\0';
+	getcwd(cwd, 255);
 	return cwd;
 }
 
@@ -73,6 +79,7 @@ char **get_args(char *input, int *num_args) {
 	char *arg = strtok(input, " \n\r\t");
 	int counter;
 	for (counter = 0; arg; counter++) {
+		if (!strcmp(arg, "~")) arg = get_home_dir();
 		args[counter] = arg;
 		arg = strtok(NULL, " \n\r\t");
 	}
@@ -89,57 +96,36 @@ int execute(char **args, int *num_args) {
 
 	// cd (might put this someplace else)
 	if (!strcmp(args[0], "cd")) {
-		if (args[1] && strcmp(args[1], "~")) { // modify ~ ?
-			if (chdir(args[1])) {
-					fprintf(stderr, "%s\n", strerror(errno));
-			}
+		if (args[1]) {
+			if (chdir(args[1])) perror("shell");
 		}
-		else {
-			char *home = get_home_dir();
-			chdir(home);
-		}
+		else chdir(get_home_dir());
 		return 1;
 	}
 
 	// exit (this too)
-	if (!strcmp(args[0], "exit")) {
-		return 0;
-	}
+	if (!strcmp(args[0], "exit")) return 0;
 
 	// system binaries
 	int f = fork();
 	if (f == 0) {
-
-		// colorized ls (Doesn't work in BSD)
-		/*
-	    if (!strcmp(args[0], "ls")) {
-	    	args[*num_args] = "--color=auto";
-	    	*num_args += 1;
- 			args[*num_args] = NULL;
-		}
-		 */
-
 		if (execvp(args[0], args) == -1) {
 			fprintf(stderr, "%s: command not found\n", args[0]);
 		}
 	}
-	else {
-		wait(NULL);
-	}
+	else wait(NULL);
+
 	if (DEBUG) fprintf(stderr, "----------------------------\n");
 	return 1;
 }
 
-int change_stdout(char **input, int *redirect, int *dup_stdout, int *new_stdout) {
+int change_stdout(char *input, int *redirect, int *dup_stdout, int *new_stdout) {
 	char *new_stdout_path;
-	int append_flag = strstr(*input, ">>") ? O_APPEND : O_TRUNC ;
+	int append_flag = strstr(input, ">>") ? O_APPEND : O_TRUNC;
 
-	char *input_dup = *input;
-	*input = strsep(&input_dup, ">");
-	if (DEBUG) fprintf(stderr, "new input: ~|%s|~\n", *input);
-
-	new_stdout_path = strtok(input_dup, ">");
-	new_stdout_path = strtok(new_stdout_path, " \n\r\t");
+	char *input_dup = strdup(input);
+	strsep(&input_dup, ">");
+	new_stdout_path = strtok(input_dup, " \n\r\t");
 
 	if (DEBUG) fprintf(stderr, "new_stdout_path: ~|%s|~\n", new_stdout_path);
 	if (new_stdout_path) {
@@ -149,22 +135,19 @@ int change_stdout(char **input, int *redirect, int *dup_stdout, int *new_stdout)
 		*new_stdout = open(new_stdout_path, O_WRONLY | append_flag | O_CREAT , 0644);
 	}
 	else {
-		fprintf(stderr, "shell: File to redirect to not specified\n");
+		fprintf(stderr, "shell: OUT file not specified\n");
 		return 0;
 	}
 	if (DEBUG) fprintf(stderr, "redirect after >: %d\n", *redirect);
 	return 1;
 }
 
-int change_stdin(char **input, int *redirect, int *dup_stdin, int *new_stdin) {
+int change_stdin(char *input, int *redirect, int *dup_stdin, int *new_stdin) {
 	char *new_stdin_path;
 
-	char *input_dup = *input;
-	*input = strsep(&input_dup, "<");
-	if (DEBUG) fprintf(stderr, "new input: ~|%s|~\n", *input);
-
-	new_stdin_path = strtok(input_dup, "<");
-	new_stdin_path = strtok(new_stdin_path, " \n\r\t");
+	char *input_dup = strdup(input);
+	strsep(&input_dup, "<");
+	new_stdin_path = strtok(input_dup, " \n\r\t");
 
 	if (DEBUG) fprintf(stderr, "new_stdin_path: ~|%s|~\n", new_stdin_path);
 	if (new_stdin_path) {
@@ -174,7 +157,7 @@ int change_stdin(char **input, int *redirect, int *dup_stdin, int *new_stdin) {
 		*new_stdin = open(new_stdin_path, O_RDONLY);
 	}
 	else {
-		fprintf(stderr, "shell: File to redirect from no specified\n");
+		fprintf(stderr, "shell: IN file not specified\n");
 		return 0;
 	}
 	if (DEBUG) fprintf(stderr, "redirect after <: %d\n", *redirect);
@@ -197,9 +180,7 @@ void print_args(char **args, int num_args) {
 	fprintf(stderr, "----------------------------\n");
 	fprintf(stderr, "ARGUMENTS\n");
 	int k = 0;
-	for (; k < num_args; k++) {
-		fprintf(stderr, "arg: ~|%s|~\n", args[k]);
-	}
+	for (; k < num_args; k++) fprintf(stderr, "arg: ~|%s|~\n", args[k]);
 	fprintf(stderr, "----------------------------\n");
 }
 
@@ -222,12 +203,23 @@ int process() {
 
 		single_input = strsep(&s, ";");
 		if (DEBUG) fprintf(stderr, "single_input:~|%s|~\n", single_input);
+
 		if (!single_input || !strlen(single_input)) return 1;
 
-		if (strchr(single_input, '>')) 
-			if (!change_stdout(&single_input, &redirect, &dup_stdout, &new_stdout)) return 1;
-		if (strchr(single_input, '<')) 
-			if (!change_stdin(&single_input, &redirect, &dup_stdin, &new_stdin)) return 1;
+		if (strchr(single_input, '>'))
+			if (!change_stdout(single_input, &redirect, &dup_stdout, &new_stdout)) return 1;
+
+		if (strchr(single_input, '<'))
+			if (!change_stdin(single_input, &redirect, &dup_stdin, &new_stdin)) return 1;
+
+		single_input = strsep(&single_input, "><");
+		if (DEBUG) fprintf(stderr, "new single_input:~|%s|~\n", single_input);
+
+		//piping goes here
+		int pipe_num;
+		char *p = single_input;
+		for (pipe_num = 0; p[pipe_num]; p[pipe_num]=='|' ? pipe_num++ : *p++);
+		fprintf(stderr, "pipe_num: %d\n", pipe_num);
 
 		args = get_args(single_input, &num_args);
 		if (DEBUG) print_args(args, num_args);
