0f4210b06df86e3889bac4d043ac0edf34ee3a30
AWinnerIsYou <seijiyawata@gmail.com>
2016-12-02 11:44:53 -0500

    Parse, prompt header descriptions

--- parse.h
+++ parse.h
@@ -1,5 +1,20 @@
 #ifndef _PARSEH_
 #define _PARSEH_
+/*======== char *read_raw() ==========
+ * Inputs: none
+ * Returns: Dynamically allocated string from terminal input
+ *
+ * Dynamically allocates a buffer for use throughout the shell
+ * If Ctrl-D is pressed, the program exits
+ * ====================*/
 char *read_raw(); 
+/*======== char **get_args() ==========
+ * Inputs: char *input
+ *         int *num_args
+ * Returns: Array of strings, where each entry is a whitespace separated token of the user input
+ *
+ * Dynamically allocates a buffer for use throughout the shell
+ * If an argument is "~", it gets replaced with the home directory.
+ * ====================*/
 char **get_args(char *input, int *num_args);
 #endif
--- prompt.h
+++ prompt.h
@@ -1,9 +1,41 @@
 #ifndef _PROMPTH_
 #define _PROMPTH_
+
+/*======== char *get_home_dir() ==========
+Inputs: none 
+Returns: Home directory string
+
+Get environment variable "HOME"
+====================*/
 char *get_home_dir();
+/*======== char *get_username() =========
+Inputs: none 
+Returns: User string
+
+Get environment variable "USER"
+====================*/
 char *get_username();
+/*======== char *get_hostname() =========
+Inputs: none
+Returns: Hostname string
+
+Dynamically allocates buffer with gethostname()
+====================*/
 char *get_hostname();
+
+/*======== char *get_cwd() =========
+Inputs: none
+Returns: CWD string
+
+Dynamically allocated buffer with getcwd()
+====================*/
 char *get_cwd();
 
+/*======== void print_prompt() =========
+Inputs: none
+Returns: none
+
+Prints shell prompt and frees cwd and hostname memory
+====================*/
 void print_prompt();
 #endif
